df0b02a9c8b0a213606b3f98d059754513e1dff2
Mohini <mohini@Mohinis-MacBook-Air.local>
2025-05-28 15:34:05 +0530

    🗃️ Added Article model and admin registration

--- ContexQ/articles/models.py
+++ ContexQ/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.TextField()
+    published = models.CharField(max_length=100)
+    source = models.CharField(max_length=200)
+    summary = models.TextField(blank=True, null=True)
+    link = models.URLField(unique=True)
+    country = models.CharField(max_length=100)
+    language = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.title} [{self.country}]"
